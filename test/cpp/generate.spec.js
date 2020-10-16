1e8c3d8386faaf67c80bb1709e673e62ff501e4d
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2020-10-16 21:56:41 +0300

    feat: Add 5.0.3-stableid.json
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- test/cpp/generate.spec.js
+++ test/cpp/generate.spec.js
@@ -7,6 +7,7 @@ const stableIDs4083 = require('../../assets/4.8.3-stableid.json')
 const stableIDs4102 = require('../../assets/4.10.2-stableid.json')
 const stableIDs4114 = require('../../assets/4.11.4-stableid.json')
 const stableIDs4120 = require('../../assets/4.12.0-stableid.json')
+const stableIDs5003 = require('../../assets/5.0.3-stableid.json')
 
 test('Generates c++ code for Sc2 v4.8.3', () => {
   generate(stableIDs4083)
@@ -28,6 +29,12 @@ test('Generates c++ code for Sc2 v4.11.4', () => {
 
 test('Generates c++ code for Sc2 v4.12.0', () => {
   generate(stableIDs4120)
+  expect(fs.generatedHeader).toMatchSnapshot()
+  expect(fs.generatedCPP).toMatchSnapshot()
+})
+
+test('Generates c++ code for Sc2 v5.0.3', () => {
+  generate(stableIDs5003)
   expect(fs.generatedHeader).toMatchSnapshot()
   expect(fs.generatedCPP).toMatchSnapshot()
 })
